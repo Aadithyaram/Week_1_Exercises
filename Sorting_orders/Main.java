e37774bb5ace161a4e1fd93925f0783064c6e8c3
Aadithyaram <deepuaadithyaram@gmail.com>
2024-07-31 14:46:07 +0530

    final commit

--- Sorting_orders/Main.java
+++ Sorting_orders/Main.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Order[] orders = {
-            new Order("OR001", "Alice", 250.75),
-            new Order("OR002", "Bob", 150.50),
-            new Order("OR003", "Charlie", 300.20),
-            new Order("OR004", "David", 100.90)
+            new Order("OR001", "Aadi", 250.75),
+            new Order("OR002", "Jude", 150.50),
+            new Order("OR003", "Venkat", 300.20),
+            new Order("OR004", "Dem", 100.90)
         };
 
         SortOrders sortOrders = new SortOrders();
@@ -20,10 +20,10 @@ public class Main {
 
         
         Order[] ordersForQuickSort = {
-            new Order("OR001", "Alice", 250.75),
-            new Order("OR002", "Bob", 150.50),
-            new Order("OR003", "Charlie", 300.20),
-            new Order("OR004", "David", 100.90)
+            new Order("OR001", "Aadi", 250.75),
+            new Order("OR002", "Jude", 150.50),
+            new Order("OR003", "Venkat", 300.20),
+            new Order("OR004", "Dem", 100.90)
         };
         sortOrders.quickSort(ordersForQuickSort, 0, ordersForQuickSort.length - 1);
         System.out.println("Quick Sort Result: " + Arrays.toString(ordersForQuickSort));
